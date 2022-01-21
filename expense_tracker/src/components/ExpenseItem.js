70fff00810ea2436cff68451917d586faa960b9a
cmulliss <cmulliss@gmail.com>
2022-01-21 17:45:24 +0000

    nesting

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+import Card from './Card'
 
-const ExpenseItem = (props) => {
+function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log('Clicked')
+  }
+
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>£{props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   )
 }
 
 export default ExpenseItem
+
+// can add fn to button on the fly, as an anonymous fn, easy to add event listener, but adds a lot of code to jsx block
+{
+  /* <button
+        onClick={() => {
+          console.log('Clicked')
+        }}
+      > */
+}
